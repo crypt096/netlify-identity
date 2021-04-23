81fb37ab6f0263ff6b5433bc829529da1f4e63d7
crypt0 <acvjetan@gmail.com>
2021-04-23 18:14:28 +0200

    Add netlify identity service.

--- stores/authContext.js
+++ stores/authContext.js
@@ -1,4 +1,5 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import netlifyIdentity from "netlify-identity-widget";
 
 const AuthContext = createContext({
   user: null,
@@ -9,6 +10,11 @@ const AuthContext = createContext({
 
 export const AuthContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    // Init Netlify identity
+    netlifyIdentity.init();
+  }, []);
   return <AuthContext.Provider value={user}>{children}</AuthContext.Provider>;
 };
 
